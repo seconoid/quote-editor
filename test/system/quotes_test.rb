3096d33a1f088daf73397fc4873bd8bee1e3ffd6
seconoid <seconsecond@gmail.com>
2022-07-20 22:32:09 +0900

    Add turbo frame test

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,12 +2,8 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = quotes(:first)
   end
-
-  # ...
-  # The test we just wrote
-  # ...
 
   test "Showing a quote" do
     visit quotes_path
@@ -16,14 +12,28 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: @quote.name
   end
 
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
