d65be838d190086a62df27bb0a74fc9683e5e87d
seconoid <seconsecond@gmail.com>
2022-07-22 00:00:57 +0900

    Three callbacks are equivalent toa single line and finish chapter 5

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,5 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
