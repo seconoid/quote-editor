2d4aeeebe4db3ff8a3272b34411c953f31b36af7
seconoid <seconsecond@gmail.com>
2022-07-21 23:48:50 +0900

    Add realtime crud with turbo stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,8 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
