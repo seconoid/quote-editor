834a5cb371300c31b47ca9511adbaf64c9d15cf8
seconoid <seconsecond@gmail.com>
2022-07-27 23:15:17 +0900

    Add flash at create quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,7 +18,7 @@ class QuotesController < ApplicationController
     if @quote.save
       respond_to do |format|
         format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = "Quote was successfully created." }
       end
     else
       render :new, status: :unprocessable_entity
