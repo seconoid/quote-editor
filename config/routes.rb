1620733d8fac3f75974753bd8988d213fa2c9ed4
seconoid <seconsecond@gmail.com>
2022-08-04 22:34:02 +0900

    Show line items at show quote

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
